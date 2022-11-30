e4dce52ec75a5851bf331347e6d966f3b05140e7
alent07 <alentmichael8592@gmail.com>
2022-11-30 18:11:26 +0530

    arr11.commit

---
+++ arr11.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int i=4;
+string fruits[4]={"apple","orange","grape","banana"};
+for(i=0;i<4;i++)
+{
+cout<<i<<"="<<fruits[i]<<"\n";
+}
+
+}
